6fcce4354dda43ec22238fa8e0582ede95c70f00
rwiley89 <rwiley@fullsail.edu>
2015-05-07 12:16:23 -0400

    Day Two Commit
    Cheah.

---
+++ DayTwo/js/script.js
@@ -0,0 +1,21 @@
+/*
+ Richard Wiley-Conille
+ SDI 1505
+ Day Two
+ Fun with Data Types
+*/
+
+//Variables
+var myHomeAddress    = "123 Elm Street";
+var milesToStore     = "8.5";
+var construction;
+var myFavoriteStore = "Publix Supermarket";
+
+//adding confirm()
+construction = confirm("Is there construction? Click OK for yes, Cancel for no.");
+
+//my outputs
+console.log("I live at " + myHomeAddress + "."); //first output
+console.log("I shop at " + myFavoriteStore + ".");
+console.log("It's only " + milesToStore + "to the store.");
+console.log("It's " + construction + " that there is construction enroute.");
